f80c5f0b33e68156211b136d86a0cdaf8c9865bc
kanadara13 <jaehyeok.shin@leisureq.com>
2018-12-01 23:00:00 +0900

    [test] 문구 추가

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const words =
         {id : "1", writer : "소크라테스", word :"너 자신을 알라" ,image : "https://pasteve.com/wp-content/uploads/2018/01/shutterstock_751011886-min.jpg"},
         {id : "2", writer : "지각생", word :"늦었다하면 진짜 늦었다" ,image : ""},
         {id : "3", writer : "회사원", word :"퇴근 시간은 내 마음속에 있다" ,image : ""},
-        {id : "4", writer : "???", word : "" ,image : ""}
+        {id : "4", writer : "???", word : "???" ,image : ""}
     ]
 
 class App extends Component {
