246995ad90e2b290f4fce4fe7fe1b92aa68afd7e
kanadara13 <jaehyeok.shin@leisureq.com>
2018-12-01 23:12:01 +0900

    Updates

---
+++ precache-manifest.b596dd7baabdb25ea4108aa1e95141e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48d5410470fb1dbf9edc",
+    "url": "/word_app/static/js/runtime~main.48d54104.js"
+  },
+  {
+    "revision": "91908b82aa6ef4b11bf5",
+    "url": "/word_app/static/js/main.91908b82.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/word_app/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "91908b82aa6ef4b11bf5",
+    "url": "/word_app/static/css/main.32891547.chunk.css"
+  },
+  {
+    "revision": "aabc6e3398015747be43844647d95db1",
+    "url": "/word_app/index.html"
+  }
+];
