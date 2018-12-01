4376514fcf5a9e517a1f78864ad18b9ec34a752c
kanadara13 <jaehyeok.shin@leisureq.com>
2018-12-01 22:56:51 +0900

    [test] 문구 추가

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Word from './Word'
 const words =
     [
         {id : "1", writer : "소크라테스", word :"너 자신을 알라" ,image : "https://pasteve.com/wp-content/uploads/2018/01/shutterstock_751011886-min.jpg"},
-        {id : "2", writer : "b", word :"늦었다하면 진짜 늦었다" ,image : ""},
-        {id : "3", writer : "c", word :"" ,image : ""},
-        {id : "4", writer : "d", word :"" ,image : ""}
+        {id : "2", writer : "지각생", word :"늦었다하면 진짜 늦었다" ,image : ""},
+        {id : "3", writer : "회사원", word :"퇴근 시간은 내 마음속에 있다" ,image : ""},
+        {id : "4", writer : "???", word : "" ,image : ""}
     ]
 
 class App extends Component {
