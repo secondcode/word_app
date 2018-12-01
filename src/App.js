54d6a6d5aacb15e491409ccb28741e3499a21578
kanadara13 <jaehyeok.shin@leisureq.com>
2018-12-01 23:16:36 +0900

    json homepage fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const words =
     [
         {id : "1", writer : "소크라테스", word :"너 자신을 알라" ,image : "https://pasteve.com/wp-content/uploads/2018/01/shutterstock_751011886-min.jpg"},
         {id : "2", writer : "지각생", word :"늦었다하면 진짜 늦었다" ,image : ""},
-        {id : "3", writer : "회사원", word :"퇴근 시간은 내 마음속에 있다" ,image : ""},
+        {id : "3", writer : "회사원", word :"퇴근 시간은 내 마음속에 있다" ,image : "https://redtea.kr/img/fri/6.jpg"},
         {id : "4", writer : "???", word : "???" ,image : ""}
     ]
 
